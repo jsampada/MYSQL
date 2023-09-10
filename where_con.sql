617196a2ecb30f90419846340d0fb7b0fec7adf6
jsampada <97596165+jsampada@users.noreply.github.com>
2023-09-10 23:06:29 +0530

    Add files via upload

---
+++ where_con.sql
@@ -0,0 +1,5 @@
+use workspace;
+#select * from company where emp_id=1;
+#select * from company where emp_salary=27000 and emp_name='sampada';
+#select * from company where emp_salary=27000 OR emp_name='sampada';
+select * from company where emp_salary!=26000;
