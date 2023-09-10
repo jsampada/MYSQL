4d4cbc8dfa5fc268d7e523f305bd0a9062080a38
jsampada <97596165+jsampada@users.noreply.github.com>
2023-09-10 21:41:22 +0530

    Add files via upload

---
+++ create_company.sql
@@ -0,0 +1,10 @@
+# create database workspace;
+#show databases;
+#use workspace;
+#create table company (emp_id int,emp_name varchar(25),emp_salary int);
+#insert into company values(1,'sampada',27000)
+#insert into company values(2,'sakshi',24000),(3,'rutuja',27000)
+#insert into company values(4,'priya',24000),(5,'diksha',26000),(6,'rohini',28000),(7,'pratiksha',26000),(8,'sneha',27000)
+#SHOW TABLES;
+#desc company;
+#select* from company;
